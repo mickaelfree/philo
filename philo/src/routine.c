822f91102dc48094eece1c464ee8b1838521073c
mickmart <mickmart@student.42lyon.fr>
2025-09-08 20:32:46 +0200

    fix some erreur

---
+++ philo/src/routine.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mickmart <mickmart@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 20:06:59 by mickmart          #+#    #+#             */
+/*   Updated: 2025/09/08 20:18:19 by mickmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+static void	handle_single_philo(t_philo *philo, t_simu *simu)
+{
+	pthread_mutex_lock(&simu->forks[philo->left_fork]);
+	print_action(simu, philo->id, TAKING_FORK);
+	ft_sleep(simu->time_to_die + 1);
+	pthread_mutex_unlock(&simu->forks[philo->left_fork]);
+}
+
+static void	take_forks(t_philo *philo, t_simu *simu)
+{
+	int	first_fork;
+	int	second_fork;
+
+	first_fork = philo->left_fork;
+	second_fork = philo->right_fork;
+	if (philo->id % 2 == 0)
+	{
+		first_fork = philo->right_fork;
+		second_fork = philo->left_fork;
+	}
+	pthread_mutex_lock(&simu->forks[first_fork]);
+	simu->fork_status[first_fork] = 1;
+	print_action(simu, philo->id, TAKING_FORK);
+	pthread_mutex_lock(&simu->forks[second_fork]);
+	simu->fork_status[second_fork] = 1;
+	print_action(simu, philo->id, TAKING_FORK);
+}
+
+void	philo_eat(t_philo *philo, t_simu *simu)
+{
+	if (simu->nb_philo == 1)
+	{
+		handle_single_philo(philo, simu);
+		return ;
+	}
+	take_forks(philo, simu);
+	pthread_mutex_lock(&philo->meal_mutex);
+	philo->last_meal = get_time_ms();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(&philo->meal_mutex);
+	print_action(simu, philo->id, EAT);
+	ft_sleep(simu->time_to_eat);
+	simu->fork_status[philo->left_fork] = 0;
+	pthread_mutex_unlock(&simu->forks[philo->left_fork]);
+	simu->fork_status[philo->right_fork] = 0;
+	pthread_mutex_unlock(&simu->forks[philo->right_fork]);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+	t_simu	*simu;
+
+	philo = (t_philo *)arg;
+	simu = philo->simu;
+	if (philo->id % 2 == 0)
+		ft_sleep(10);
+	while (!simu->simulation_end)
+	{
+		philo_eat(philo, simu);
+		print_action(simu, philo->id, SLEEP);
+		ft_sleep(simu->time_to_sleep);
+		print_action(simu, philo->id, THINK);
+	}
+	return (NULL);
+}
